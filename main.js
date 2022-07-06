f927704eb8ba169c39db0699e9ba57c086ebd8e0
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-07-06 12:52:24 +0200

    .

---
+++ main.js
@@ -0,0 +1,24 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        index : 0,
+        todos: [
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare il bucato',
+                done: false
+            }
+        ]
+    },
+
+    methods: {
+        
+    }
+})
